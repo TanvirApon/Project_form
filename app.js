4c19e8bbb87dfbc1f0f7c1049d81f9405db4ac7d
tanvirhossainapon <tanvirhossainapon@gmail.com>
2024-10-05 21:58:19 +0600

    Code Updated

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 console.log("Javascript file added");
 
-  var submit = document.getElementById("submission");
+ var submit = document.getElementById("submission");
+ var add  = document.getElementById("addition");
+//  var close = document.getElementById("close");
+function closenav() {
+  showProduct.removeChild(newDiv);
+}
 
+// show product div
+add.addEventListener( "click",()=>{
+ 
+  const showProduct = document.getElementById("product");
+  const newDiv = document.createElement("div");
+
+   newDiv.innerHTML =  `
+              <div class="d-flex justify-content-end" onclick="closenav();">
+               <button type="button" class="btn-close" aria-label="Close"></button>
+              </div> 
+               <div class="mb-3">
+                  <label class="mb-2">Product Name : </label>
+                  <input type="text" class="form-control" id="pname">
+              </div>
+              <div class="mb-3">
+                  <label class="mb-2">Description : </label>
+                  <textarea class="form-control" id="description" rows="3"></textarea>
+              </div>
+              <div class="mb-3">
+                  <label class="mb-2">Quantity : </label>
+                  <input type="number" class="form-control" id="quantity">
+              </div>
+              <div class="mb-3">
+                  <label class="mb-2">Price : </label>
+                  <input type="number" class="form-control" id="price">
+              </div>
+              <div class="mb-3">
+                  <label class="mb-2">Rate : </label>
+                  <input type="number" class="form-control" id="rate">
+              </div>
+              <div class="mb-3">
+                  <label class="mb-2">Amount : </label>
+                  <input type="number" class="form-control" id="amount">
+              </div>
+            `;
+
+   showProduct.appendChild(newDiv);
+
+
+ 
+})
+
+// submit button on click event listener
 submit.addEventListener("click", () => {
 
   // var buyer = [];
@@ -20,15 +68,12 @@ submit.addEventListener("click", () => {
 
   console.log(buyerDetails);
 
-
-
   var productName = document.getElementById("pname").value;
   var description = document.getElementById("description").value;
   var quantity = document.getElementById("quantity").value;
   var price = document.getElementById("price").value;
   var rate = document.getElementById("rate").value;
   var amount = document.getElementById("amount").value;
-
 
   var productDetails = {
     "Product's name": productName,
@@ -38,15 +83,20 @@ submit.addEventListener("click", () => {
     "Rate": rate,
     "Amount": amount
   }
-
+  
   product.push(productDetails);
-  console.log(product)
-
-
-
+  console.log(product);
 })
 
 
+//close button 
+// close.addEventListener("click",()=>{
+//   console.log("close button has been clicked");
 
+// })
+
+function close(){
+  console.log("close button has been clicked");
+}
 
 
