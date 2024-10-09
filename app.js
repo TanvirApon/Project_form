24a88e1358da1f86114846a29c4b301b787ebd2d
TanvirApon <tanvirhossainapon@gmail.com>
2024-10-09 09:31:05 +0600

    Last Updated

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-console.log("Javascript file added");
-
  var submit = document.getElementById("submission");
- console.log(typeof(submit))
  var add  = document.getElementById("addition");
- console.log(typeof(add))
- 
  var product =  document.getElementById("product");
- console.log(typeof(product))
- console.log(product)
 
 // show product div
 add.addEventListener( "click",()=>{
- 
-
   var showProduct = document.getElementById("products");
   var productTemplate = document.querySelector(".product.d-none").cloneNode(true);
   productTemplate.classList.remove("d-none");
   productTemplate.classList.add("d-block");
-   
-  
 
   showProduct.appendChild(productTemplate);
-
   const close = productTemplate.querySelector(".btn-close");
-
    close.addEventListener("click",()=>{
      productTemplate.remove();
    });
@@ -32,23 +19,17 @@ add.addEventListener( "click",()=>{
 
 // submit button on click event listener
 submit.addEventListener("click", () => {
-
-  // var buyer = []; 
   var products = [];
   var productJson ;
-
   var buyerName = document.getElementById("buyer_name").value;
   var buyerPhone = document.getElementById("buyer_phone").value;
   var area = document.getElementById("select").value;
-
   var buyerDetails = {
     "Buyer's name": buyerName,
     "Buyer's Phone": buyerPhone,
     "Area": area
   }
-
   console.log(buyerDetails);
-  
   var productInfo = document.querySelectorAll(".product.d-block");
   console.log("Product Information: ", productInfo);
   console.log("Product Information: ", productInfo);
@@ -60,8 +41,7 @@ submit.addEventListener("click", () => {
       var quantity = parseFloat(productInfo[i].querySelector(".p-quantity").value);
       var price = parseFloat(productInfo[i].querySelector(".p-price").value);
       var rate = parseFloat(productInfo[i].querySelector(".p-rate").value);
-      var amount = parseFloat(productInfo[i].querySelector(".p-amount").value);
-     
+      var amount = parseFloat(productInfo[i].querySelector(".p-amount").value);   
           var productDetails = {
             "Product's name": productName,
             "Description": description,
@@ -70,16 +50,13 @@ submit.addEventListener("click", () => {
             "Rate": rate,
             "Amount": amount
         };
-
     products.push(productDetails);
   }
-  
   productJson = JSON.stringify(products);
-
   console.log(productJson);
-
+  
   var display = document.querySelector(".Json-display");
-  display.innerHTML = productJson + "<br>";
+  display.innerHTML = productJson;
 })
 
 
